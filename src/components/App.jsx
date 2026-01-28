a26a404958f34a3d4d4a14f15b9636711f7ed022
Osva-Dev <osvadev30@gmail.com>
2026-01-27 23:45:02 -0600

    add new AboutMe components with Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,23 @@ import AboutMe from "../components/AboutMe/AboutMe.jsx";
 import Reviews from "../components/Reviews/Reviews.jsx";
 import AboutUs from "../components/AboutUs/AboutUs.jsx";
 
+import Contact from "../components/AboutMe/Contact.jsx";
+import Hobbies from "../components/AboutMe/Hobbies.jsx";
+import MyStory from "../components/AboutMe/MyStory.jsx";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="about-me" element={<AboutMe />} />
+
+        <Route path="about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+
         <Route path="reviews" element={<Reviews />} />
         <Route path="about-us" element={<AboutUs />} />
       </Routes>
