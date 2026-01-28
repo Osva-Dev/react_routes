cfa216c414d3081ea538d1ce1deba68f3c2af08e
Osva-Dev <osvadev30@gmail.com>
2026-01-27 21:35:17 -0600

    routing react first advance

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header/Header";
+import Dashboard from "../components/Dashboard/Dashboard.jsx";
+import AboutMe from "../components/AboutMe/AboutMe.jsx";
+import Reviews from "../components/Reviews/Reviews.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="aboutme" element={<AboutMe />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 }
