44f3e8efc48944bc5bf850f63c78655629729a29
Osva-Dev <osvadev30@gmail.com>
2026-01-28 22:59:59 -0600

    add dinamic rutes Reviews

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Header from "./Header/Header";
 import Dashboard from "../components/Dashboard/Dashboard.jsx";
 import AboutMe from "../components/AboutMe/AboutMe.jsx";
@@ -13,12 +16,35 @@ import MyStory from "../components/AboutMe/MyStory.jsx";
 import SiteHistory from "../components/AboutUs/SiteHistory.jsx";
 import SiteMission from "../components/AboutUs/SiteMission.jsx";
 
+import Review from "../components/Review/Review.jsx";
+
 function App() {
+  const [reviews, setReviews] = useState([]); // Nueva variable useState
+
+  useEffect(() => {
+    // Obtiene los datos de reseñas del servidor.
+    fetch("https://emoji-critic.es.tripleten-services.com/v1/reviews")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Pasa el cuerpo de la respuesta analizada a la función setter.
+        setReviews(data);
+      })
+      .catch(console.error);
+    // Un array de dependencia vacío significa que el hook sólo se ejecuta cuando
+    // se carga el componente.
+  }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/:reviewId"
+          element={<Review reviews={reviews} />}
+        />
 
         <Route path="about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
